0181af15dc1f5ef2ca89854378253f1f8d4fe50e
20121bsi0473 <20121bsi0473@SR6182.cefetes.br>
2014-07-09 11:42:21 -0300

    primeiro commit

---
+++ PadraoTemplateMethod/src/main/java/br/edu/ifes/App/App.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifes.App;
+
+/**
+ *
+ * @author 20121bsi0473
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Ikmplementar O exemplo 6, Usando uma Classe Pai principal que
+        //dara as coordenadas para os filhos terminarem a execução
+        //Ou seja, Definir um esquelo funcional de um código, deixando os filhos
+        //redefinam certos passos
+        
+    }
+}
---
+++ PadraoTemplateMethod/src/main/java/br/edu/ifes/Model/Codecs.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifes.Model;
+
+/**
+ *
+ * @author 20121bsi0473
+ */
+public enum Codecs {
+    videoMP3, videoAVI, videoMP4;
+}
